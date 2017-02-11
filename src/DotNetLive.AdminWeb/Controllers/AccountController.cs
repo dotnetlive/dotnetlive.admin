de8786e270365a5a68d17438dbbc79121750ab09
Duke Cheng <dk@feinian.me>
2017-02-11 10:29:24 +0800

    Init admin web

---
+++ src/DotNetLive.AdminWeb/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+using DotNetLive.Framework.UserIdentity;
+using DotNetLive.Framework.WebFramework.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace DotNetLive.AdminWeb.Controllers
+{
+    [Authorize]
+    public class AccountController : BaseAccountController
+    {
+        public AccountController(IOptions<SecuritySettings> securitySetting) : base(securitySetting)
+        {
+
+        }
+    }
+}
